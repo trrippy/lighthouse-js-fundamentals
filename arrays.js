3972dfc2655ccc9e480064787a5484d88945061d
Wesley Wong <wes.wong@hotmail.com>
2016-10-25 22:43:42 -0700

    Adds conditonals exercises

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[6]);
+console.log(iceCreamFlavours.length);
